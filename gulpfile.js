0c3053c68fc88ad212ffd41e061488130dce82b9
Thomas Bjørndahl <thomas.bjorndahl@fmcti.com>
2017-01-03 19:10:23 +0100

    Added bootstrap ++

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 var gulp = require('gulp');
 var ren = require('gulp-rename');
 var nm = 'node_modules';
-var scripts = 'src/scripts';
+
+var folders = {
+    scripts: 'src/scripts',
+    style: 'src/css'
+};
 
 gulp.task('default', ['copyScripts'], function() {
   // place code for your default task here
   
 });
 
-gulp.task('copyScripts',['copyRequire']);
+gulp.task('copyScripts',['copyRequire','copyAngular','copyBootstrap']);
 
 gulp.task('copyRequire', function(){
     return gulp.src(nm + '/requirejs/bin/r.js')
         .pipe(ren('require.js'))
-        .pipe(gulp.dest(scripts + '/requirejs/'));
+        .pipe(gulp.dest(folders.scripts + '/requirejs/'));
+});
+
+gulp.task('copyAngular', function(){
+    return gulp.src(nm + '/angular/angular.js')
+        .pipe(gulp.dest(folders.scripts + '/angular/'));
+});
+
+gulp.task('copyBootstrap', function(){
+    return gulp.src(nm + '/bootstrap/dist/**/*.*')
+        .pipe(gulp.dest(folders.style + '/bootstrap/'));
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,10 @@
-requirejs(["helper/util"], function(util) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
+requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        app: '../app'
+    }
 });
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['app/features']);
